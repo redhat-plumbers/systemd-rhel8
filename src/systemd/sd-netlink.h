3c31ef05ba732e3ab5d23761c5f84768ca8de68e
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-12-15 21:32:25 +0900

    sd-netlink: introduce sd_netlink_message_read_strv()
    The combination of sd_netlink_message_enter_container() and
    sd_netlink_message_read_string() only reads the last element if the attribute is
    duplicated, such a situation easily happens for IFLA_ALT_IFNAME.
    The function introduced here reads all matched attributes.
    
    (cherry picked from commit 8f3c1859669230c2c8458675f41de13e369b47e7)
    
    Related: #1850986

--- src/systemd/sd-netlink.h
+++ src/systemd/sd-netlink.h
@@ -82,6 +82,7 @@ int sd_netlink_message_open_container_union(sd_netlink_message *m, unsigned shor
 int sd_netlink_message_close_container(sd_netlink_message *m);
 
 int sd_netlink_message_read_string(sd_netlink_message *m, unsigned short type, const char **data);
+int sd_netlink_message_read_strv(sd_netlink_message *m, unsigned short container_type, unsigned short type_id, char ***ret);
 int sd_netlink_message_read_u8(sd_netlink_message *m, unsigned short type, uint8_t *data);
 int sd_netlink_message_read_u16(sd_netlink_message *m, unsigned short type, uint16_t *data);
 int sd_netlink_message_read_u32(sd_netlink_message *m, unsigned short type, uint32_t *data);

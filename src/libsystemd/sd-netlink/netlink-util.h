7ba9c8305b1e5b8c22a8d7bd3f3068c16097557c
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-12-15 23:21:18 +0900

    udev: support AlternativeName= setting in .link file
    (cherry picked from commit a5053a158b43c5ddee90f4915b9fc603e0191d6d)
    
    Related: #2005008

--- src/libsystemd/sd-netlink/netlink-util.h
+++ src/libsystemd/sd-netlink/netlink-util.h
@@ -38,6 +38,7 @@ static inline bool rtnl_message_type_is_routing_policy_rule(uint16_t type) {
 
 int rtnl_set_link_name(sd_netlink **rtnl, int ifindex, const char *name);
 int rtnl_set_link_properties(sd_netlink **rtnl, int ifindex, const char *alias, const struct ether_addr *mac, uint32_t mtu);
+int rtnl_set_link_alternative_names(sd_netlink **rtnl, int ifindex, char * const *alternative_names);
 
 int rtnl_log_parse_error(int r);
 int rtnl_log_create_error(int r);

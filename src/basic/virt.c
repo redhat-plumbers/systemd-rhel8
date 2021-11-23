0fa5137c10f02723f6987793f3165cf1a4b2bda6
Boqun Feng <boqun.feng@gmail.com>
2021-11-23 15:09:26 +0800

    virt: Fix the detection for Hyper-V VMs
    Use product_version instead of product_name in DMI table and the string
    "Hyper-V" to avoid misdetection.
    
    Fixes: #21468
    
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>
    (cherry picked from commit 76eec0649936d9ae2f9087769f463feaf0cf5cb4)
    
    Related: #2178002

--- src/basic/virt.c
+++ src/basic/virt.c
@@ -139,7 +139,8 @@ static int detect_vm_dmi(void) {
                 "/sys/class/dmi/id/product_name", /* Test this before sys_vendor to detect KVM over QEMU */
                 "/sys/class/dmi/id/sys_vendor",
                 "/sys/class/dmi/id/board_vendor",
-                "/sys/class/dmi/id/bios_vendor"
+                "/sys/class/dmi/id/bios_vendor",
+                "/sys/class/dmi/id/product_version" /* For Hyper-V VMs test */
         };
 
         static const struct {
@@ -157,7 +158,7 @@ static int detect_vm_dmi(void) {
                 { "Parallels",     VIRTUALIZATION_PARALLELS },
                 /* https://wiki.freebsd.org/bhyve */
                 { "BHYVE",         VIRTUALIZATION_BHYVE     },
-                { "Microsoft",           VIRTUALIZATION_MICROSOFT },
+                { "Hyper-V",             VIRTUALIZATION_MICROSOFT },
         };
         unsigned i;
         int r;

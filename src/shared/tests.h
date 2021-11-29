084c449c31b14e65b1640c0a4e91b1660a0faa6c
Frantisek Sumsal <frantisek@sumsal.cz>
2021-11-29 13:00:21 +0100

    tests: add helper function to autodetect CI environments
    Sadly there is no standarized way to check if we're running in some
    CI environment. So let's try to gather the heuristics in one helper
    function.
    
    (cherry picked from commit 6fbbf368f5a6d181b21f448255d5a4182dc2ab3a)
    
    Related: #2039327

--- src/shared/tests.h
+++ src/shared/tests.h
@@ -5,3 +5,6 @@ char* setup_fake_runtime_dir(void);
 bool test_is_running_from_builddir(char **exedir);
 const char* get_testdata_dir(void);
 void test_setup_logging(int level);
+
+/* Provide a convenient way to check if we're running in CI. */
+const char *ci_environment(void);

d4dd447efee22e006f3949db07dff05dfea6e58e
Frantisek Sumsal <frantisek@sumsal.cz>
2021-12-15 12:15:19 +0100

    test: make TEST-47 less racy
    Based on:
      - 2e7090e94d0c8b31d418555ab2f6a9b75318f6a4
      - e00e2e0b50bbd120290572c8d1242703fb98b34e
      - 197298ff9fc930de450330095cc5b67d165d0801
    
    Related: #2017033
    (cherry picked from commit de7125dcfe6d6c8af05262ab786f9fe7cbf15113)
    
    Related: #2039326
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- test/TEST-47-ISSUE-14566/testsuite.sh
+++ test/TEST-47-ISSUE-14566/testsuite.sh
@@ -6,11 +6,13 @@ systemd-analyze log-level debug
 systemd-analyze log-target console
 
 systemctl start issue_14566_test
+sleep 4
 systemctl status issue_14566_test
 
 leaked_pid=$(cat /leakedtestpid)
 
 systemctl stop issue_14566_test
+sleep 4
 
 # Leaked PID will still be around if we're buggy.
 # I personally prefer to see 42.

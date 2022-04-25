1ad8be47cd41f017faa5a9ca9614cbcbe784d43b
Jacek Migacz <jmigacz@redhat.com>
2022-04-25 21:12:40 +0200

    Revert "basic: use comma as separator in cpuset cgroup cpu ranges"
    This reverts commit 9fe3b9c7165afeedcf9f31959c436bcec233bb4d.
    
    RHEL-only
    
    Resolves: #1858220

--- src/core/cgroup.c
+++ src/core/cgroup.c
@@ -687,7 +687,7 @@ static void cgroup_apply_unified_cpuset(Unit *u, CPUSet cpus, const char *name) 
         _cleanup_free_ char *buf = NULL;
         int r;
 
-        buf = cpu_set_to_range_string_kernel(&cpus);
+        buf = cpu_set_to_range_string(&cpus);
         if (!buf)
             return;
 

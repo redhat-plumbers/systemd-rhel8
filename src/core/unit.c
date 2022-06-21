systemd/systemd

core: Move 'r' variable declaration to start of unit_start()

src/core/unit.c
@@ -1851,12 +1851,13 @@ static bool unit_verify_deps(Unit *u) {
 int unit_start(Unit *u) {
         UnitActiveState state;
         Unit *following;
+        int r;
 
         assert(u);
 
         /* Check start rate limiting early so that failure conditions don't cause us to enter a busy loop. */
         if (UNIT_VTABLE(u)->test_start_limit) {
-                int r = UNIT_VTABLE(u)->test_start_limit(u);
+                r = UNIT_VTABLE(u)->test_start_limit(u);
                 if (r < 0)
                         return r;
         }

redhat-plumbers/systemd-rhel8

(cherry picked from commit 5f37c1a955e399756c4137d22f7f0f45a619f425)
Related: #2095950

src/core/unit.c
@@ -1723,12 +1723,13 @@ static bool unit_verify_deps(Unit *u) {
 int unit_start(Unit *u) {
         UnitActiveState state;
         Unit *following;
+        int r;
 
         assert(u);
 
         /* Check our ability to start early so that failure conditions don't cause us to enter a busy loop. */
         if (UNIT_VTABLE(u)->can_start) {
-                int r = UNIT_VTABLE(u)->can_start(u);
+                r = UNIT_VTABLE(u)->can_start(u);
                 if (r < 0)
                         return r;
         }
b2c54ccb55c8d49289efd2273160e5e9d4ae57b0
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-07-07 18:27:02 +0900

    time-util: fix buffer-over-run
    Fixes #23928.
    
    (cherry picked from commit 9102c625a673a3246d7e73d8737f3494446bad4e)
    
    Resolves: RHEL-18768

--- src/test/test-time-util.c
+++ src/test/test-time-util.c
@@ -446,6 +446,12 @@ int main(int argc, char *argv[]) {
         test_format_timespan(1);
         test_format_timespan(USEC_PER_MSEC);
         test_format_timespan(USEC_PER_SEC);
+
+        /* See issue #23928. */
+        _cleanup_free_ char *buf;
+        assert_se(buf = new(char, 5));
+        assert_se(buf == format_timespan(buf, 5, 100005, 1000));
+
         test_timezone_is_valid();
         test_get_timezones();
         test_usec_add();

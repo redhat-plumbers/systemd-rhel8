8230810904a2a131ad02bffdbd1ce29921d250fb
Lukas Nykryn <lnykryn@redhat.com>
2022-08-18 16:35:23 +0200

    test-procfs-util: skip test on certain errors
    Inspired by upstream bf47f71c1c
    
    RHEL-only
    Related: #2087152

--- src/shared/tests.c
+++ src/shared/tests.c
@@ -78,6 +78,18 @@ void test_setup_logging(int level) {
         log_open();
 }
 
+int log_tests_skipped(const char *message) {
+        log_notice("%s: %s, skipping tests.",
+                   program_invocation_short_name, message);
+        return EXIT_TEST_SKIP;
+}
+
+int log_tests_skipped_errno(int r, const char *message) {
+        log_notice_errno(r, "%s: %s, skipping tests: %m",
+                         program_invocation_short_name, message);
+        return EXIT_TEST_SKIP;
+}
+
 const char *ci_environment(void) {
         /* We return a string because we might want to provide multiple bits of information later on: not
          * just the general CI environment type, but also whether we're sanitizing or not, etc. The caller is

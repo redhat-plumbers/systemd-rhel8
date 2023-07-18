systemd/systemd

fstab-generator: fix error code propagation in run_generator()
Previously, some errors might be ignored.

src/fstab-generator/fstab-generator.c
@@ -1298,7 +1298,7 @@ static int determine_usr(void) {
  * with /sysroot/etc/fstab available, and then we can write additional units based
  * on that file. */
 static int run_generator(void) {
-        int r, r2 = 0, r3 = 0;
+        int r, ret = 0;
 
         r = proc_cmdline_parse(parse_proc_cmdline_item, NULL, 0);
         if (r < 0)
@@ -1319,26 +1319,39 @@ static int run_generator(void) {
         /* Always honour root= and usr= in the kernel command line if we are in an initrd */
         if (in_initrd()) {
                 r = add_sysroot_mount();
+                if (r < 0 && ret >= 0)
+                        ret = r;
 
-                r2 = add_sysroot_usr_mount_or_fallback();
+                r = add_sysroot_usr_mount_or_fallback();
+                if (r < 0 && ret >= 0)
+                        ret = r;
 
-                r3 = add_volatile_root();
-        } else
+                r = add_volatile_root();
+                if (r < 0 && ret >= 0)
+                        ret = r;
+        } else {
                 r = add_volatile_var();
+                if (r < 0 && ret >= 0)
+                        ret = r;
+        }
 
         /* Honour /etc/fstab only when that's enabled */
         if (arg_fstab_enabled) {
                 /* Parse the local /etc/fstab, possibly from the initrd */
-                r2 = parse_fstab(false);
+                r = parse_fstab(false);
+                if (r < 0 && ret >= 0)
+                        ret = r;
 
                 /* If running in the initrd also parse the /etc/fstab from the host */
                 if (in_initrd())
-                        r3 = parse_fstab(true);
+                        r = parse_fstab(true);
                 else
-                        r3 = generator_enable_remount_fs_service(arg_dest);
+                        r = generator_enable_remount_fs_service(arg_dest);
+                if (r < 0 && ret >= 0)
+                        ret = r;
         }
 
-        return r < 0 ? r : r2 < 0 ? r2 : r3;
+        return ret;
 }
 
 static int run(int argc, char **argv) {

redhat-plumbers/systemd-rhel8

(cherry picked from commit 2646f1844553b5d9c6109ad6b1b86da0b6b1f248)
Related: #2190228

src/fstab-generator/fstab-generator.c
@@ -900,7 +900,7 @@ static int determine_root(void) {
 }
 
 int main(int argc, char *argv[]) {
-        int r = 0;
+        int r, ret = 0;
 
         if (argc > 1 && argc != 4) {
                 log_error("This program takes three or no arguments.");
@@ -927,38 +927,39 @@ int main(int argc, char *argv[]) {
 
         /* Always honour root= and usr= in the kernel command line if we are in an initrd */
         if (in_initrd()) {
-                int k;
-
                 r = add_sysroot_mount();
+                if (r < 0 && ret >= 0)
+                        ret = r;
 
-                k = add_sysroot_usr_mount();
-                if (k < 0)
-                        r = k;
+                r = add_sysroot_usr_mount();
+                if (r < 0 && ret >= 0)
+                        ret = r;
 
-                k = add_volatile_root();
-                if (k < 0)
-                        r = k;
-        } else
+                r = add_volatile_root();
+                if (r < 0 && ret >= 0)
+                        ret = r;
+        } else {
                 r = add_volatile_var();
+                if (r < 0 && ret >= 0)
+                        ret = r;
+        }
 
         /* Honour /etc/fstab only when that's enabled */
         if (arg_fstab_enabled) {
-                int k;
-
                 log_debug("Parsing /etc/fstab");
 
                 /* Parse the local /etc/fstab, possibly from the initrd */
-                k = parse_fstab(false);
-                if (k < 0)
-                        r = k;
+                r = parse_fstab(false);
+                if (r < 0 && ret >= 0)
+                        ret = r;
 
                 /* If running in the initrd also parse the /etc/fstab from the host */
                 if (in_initrd()) {
                         log_debug("Parsing /sysroot/etc/fstab");
 
-                        k = parse_fstab(true);
-                        if (k < 0)
-                                r = k;
+                        r = parse_fstab(true);
+                        if (r < 0 && ret >= 0)
+                                ret = r;
                 }
         }
 
@@ -971,5 +972,5 @@ int main(int argc, char *argv[]) {
         free(arg_usr_fstype);
         free(arg_usr_options);
 
-        return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+        return ret < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
systemd/systemd

fstab-generator: call add_swap() earlier
As 'where' field will be ignored for swap entry.

src/fstab-generator/fstab-generator.c
@@ -131,6 +131,11 @@ static int add_swap(
                 return true;
         }
 
+        log_debug("Found swap entry what=%s makefs=%s growfs=%s pcrfs=%s noauto=%s nofail=%s",
+                  what,
+                  yes_no(flags & MOUNT_MAKEFS), yes_no(flags & MOUNT_GROWFS), yes_no(flags & MOUNT_PCRFS),
+                  yes_no(flags & MOUNT_NOAUTO), yes_no(flags & MOUNT_NOFAIL));
+
         r = unit_name_from_path(what, ".swap", &name);
         if (r < 0)
                 return log_error_errno(r, "Failed to generate unit name: %m");
@@ -704,6 +709,7 @@ static int parse_fstab_one(
 
         _cleanup_free_ char *what = NULL, *where = NULL, *canonical_where = NULL;
         MountPointFlags flags;
+        bool is_swap;
         int r;
 
         assert(what_original);
@@ -724,6 +730,13 @@ static int parse_fstab_one(
                 log_info("/sys/ is read-only (running in a container?), ignoring mount for %s.", what);
                 return 0;
         }
+
+        is_swap = streq_ptr(fstype, "swap");
+
+        flags = fstab_options_to_flags(options, is_swap);
+
+        if (is_swap)
+                return add_swap(source, what, options, flags);
 
         where = strdup(where_original);
         if (!where)
@@ -759,15 +772,10 @@ static int parse_fstab_one(
                         log_debug("Canonicalized what=%s where=%s to %s", what, where, canonical_where);
         }
 
-        flags = fstab_options_to_flags(options, streq_ptr(fstype, "swap"));
-
         log_debug("Found entry what=%s where=%s type=%s makefs=%s growfs=%s pcrfs=%s noauto=%s nofail=%s",
                   what, where, strna(fstype),
                   yes_no(flags & MOUNT_MAKEFS), yes_no(flags & MOUNT_GROWFS), yes_no(flags & MOUNT_PCRFS),
                   yes_no(flags & MOUNT_NOAUTO), yes_no(flags & MOUNT_NOFAIL));
-
-        if (streq_ptr(fstype, "swap"))
-                return add_swap(source, what, options, flags);
 
         bool is_sysroot = in_initrd() && path_equal(where, "/sysroot");
         /* See comment from add_sysroot_usr_mount() about the need for extra indirection in case /usr needs

redhat-plumbers/systemd-rhel8

(cherry picked from commit 256604ccddd41a16de329ff792c5f49d6750e510)
Related: #2190228

src/fstab-generator/fstab-generator.c
@@ -100,6 +100,12 @@ static int add_swap(
                 return 0;
         }
 
+
+        log_debug("Found swap entry what=%s makefs=%s growfs=%s noauto=%s nofail=%s",
+                  what,
+                  yes_no(flags & MAKEFS), yes_no(flags & GROWFS),
+                  yes_no(flags & NOAUTO), yes_no(flags & NOFAIL));
+
         r = unit_name_from_path(what, ".swap", &name);
         if (r < 0)
                 return log_error_errno(r, "Failed to generate unit name: %m");
@@ -530,15 +536,17 @@ static int parse_fstab_one(
         _cleanup_free_ char *where = NULL, *what = NULL, *canonical_where = NULL;
         const char *post;
         MountpointFlags flags;
+        bool is_swap;
         int r;
 
         assert(what_original);
-        assert(where_original);
         assert(fstype);
         assert(options);
 
         if (initrd && !mount_in_initrd(where_original, options))
                 return 0;
+
+        is_swap = streq_ptr(fstype, "swap");
 
         what = fstab_node_to_udev_node(what_original);
         if (!what)
@@ -548,6 +556,13 @@ static int parse_fstab_one(
                 log_info("Running in a container, ignoring fstab device entry for %s.", what);
                 return 0;
         }
+
+        flags = fstab_options_to_flags(options, is_swap);
+
+        if (is_swap)
+                return add_swap(what, options, flags);
+
+        assert(where_original); /* 'where' is not necessary for swap entry. */
 
         where = strdup(where_original);
         if (!where)
@@ -572,15 +587,10 @@ static int parse_fstab_one(
                         log_debug("Canonicalized what=%s where=%s to %s", what, where, canonical_where);
         }
 
-        flags = fstab_options_to_flags(options, streq_ptr(fstype, "swap"));
-
         log_debug("Found entry what=%s where=%s type=%s makefs=%s noauto=%s nofail=%s",
                     what, where, strna(fstype),
                     yes_no(flags & MAKEFS),
                     yes_no(flags & NOAUTO), yes_no(flags & NOFAIL));
-
-        if (streq(fstype, "swap"))
-                return add_swap(what, options, flags);
 
         if (initrd)
                 post = SPECIAL_INITRD_FS_TARGET;
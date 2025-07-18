cfecdf44e48224597b7c9b007631089d633101cc
Ronan Pigott <ronan@rjp.ie>
2025-07-18 15:12:31 +0200

    manager: s/deserialized_subscribed/subscribed_as_strv
    Now that this field may get populated at runtime, the deserialized name
    is misleading. Change the name to reflect its updated purpose.
    
    (cherry picked from commit e1315a621ae26473fcc9cd0d6013836f5f498d40)
    
    Related: RHEL-75081

--- src/core/manager.c
+++ src/core/manager.c
@@ -1349,7 +1349,7 @@ Manager* manager_free(Manager *m) {
         free(m->switch_root_init);
 
         sd_bus_track_unref(m->subscribed);
-        strv_free(m->deserialized_subscribed);
+        strv_free(m->subscribed_as_strv);
 
         rlimit_free_all(m->rlimit);
 
@@ -1659,8 +1659,8 @@ int manager_startup(Manager *m, FILE *serialization, FDSet *fds) {
         manager_setup_bus(m);
 
         /* Now that we are connected to all possible busses, let's deserialize who is tracking us. */
-        (void) bus_track_coldplug(m->api_bus, &m->subscribed, false, m->deserialized_subscribed);
-        m->deserialized_subscribed = strv_free(m->deserialized_subscribed);
+        (void) bus_track_coldplug(m->api_bus, &m->subscribed, false, m->subscribed_as_strv);
+        m->subscribed_as_strv = strv_free(m->subscribed_as_strv);
 
         /* Third, fire things up! */
         manager_coldplug(m);
@@ -3382,7 +3382,7 @@ int manager_deserialize(Manager *m, FILE *f, FDSet *fds) {
                         exec_runtime_deserialize_one(m, val, fds);
                 else if ((val = startswith(l, "subscribed="))) {
 
-                        if (strv_extend(&m->deserialized_subscribed, val) < 0)
+                        if (strv_extend(&m->subscribed_as_strv, val) < 0)
                                 log_oom();
                 } else {
                         ManagerTimestamp q;
